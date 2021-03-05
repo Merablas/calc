b00d779da424700342cbda7832741113f86e6613
Merablas <presck@gmail.com>
2021-03-05 12:05:52 -0400

    Version 1.0.4

--- util/__init__.py
+++ util/__init__.py
@@ -1,11 +1,9 @@
 # init for calculator package
-
+#
 import re
 
 from util.type import *
 from util.header import *
 
 # the type() method is the same name as the (util.)type module, so delete the type module name
-del type 
-
-e
+del type
